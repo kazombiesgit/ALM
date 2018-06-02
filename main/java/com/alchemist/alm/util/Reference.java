4942c45524d1e5db4f7fec0818e70657d0ef0ebe
Johnny Ricatone <xxpixelheroxx@gmail.com>
2018-06-01 22:48:44 -0400

    Adding a single line of code to fix everything. Probably.

--- main/java/com/alchemist/alm/util/Reference.java
+++ main/java/com/alchemist/alm/util/Reference.java
@@ -6,8 +6,8 @@ public class Reference
     public static final String NAME = "Alm Fluids";
     public static final String VERSION = "0.01";
     public static final String ACCEPTED_VERSIONS = "[1.12.2]";
-    public static final String CLIENT_PROXY_CLASS = "ClientProxy";
-    public static final String COMMON_PROXY_CLASS = "CommonProxy";
+    public static final String CLIENT_PROXY_CLASS = "com.alchemist.alm.proxy.ClientProxy";
+    public static final String COMMON_PROXY_CLASS = "com.alchemist.alm.proxy.CommonProxy";
 
 
 }
