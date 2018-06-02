3cc7cdf4a9b6710a670aec029b6926da123a7308
Johnny Ricatone <jrr3976@rit.edu>
2018-06-01 21:41:13 -0400

    Adding a single line of code to fix everything. Probably.

--- main/java/com/Alchemist/ALM/util/handlers/RegistryHandler.java
+++ main/java/com/Alchemist/ALM/util/handlers/RegistryHandler.java
@@ -5,8 +5,10 @@ import com.Alchemist.ALM.util.IHasModel;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber
 public class RegistryHandler
 {
     @SubscribeEvent
